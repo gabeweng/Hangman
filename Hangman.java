5115ac6d41c4747a2d3a91f84af44c1416b1733a
gabeweng <66958116+gabeweng@users.noreply.github.com>
2023-06-03 14:25:12 +0000

    Modified lose condition and track guesses

--- Hangman.java
+++ Hangman.java
@@ -1,29 +1,37 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
+// import com.theokanning.openai.*;
 public class Hangman {
+  
   private char[] word;
   private char[] guesses;
   private int guessed=0;
   private int numGuess;
+  private String letters = "";
   
   public Hangman(String word, int numGuess) {
     this.word = word.toUpperCase().toCharArray();
     this.guesses = new char[word.length()];
     Arrays.fill(guesses, '_');
+    guess(' ');
+    guess('!');
+    guess('.');
+    guess(',');
+    guessed=0;
     this.numGuess = numGuess;
   }
   public boolean guess(char c) {
     boolean correct = false;
+    letters=letters+Character.toUpperCase(c);
     for (int i = 0; i < word.length; i++) {
       if (word[i] == Character.toUpperCase(c)) {
                 guesses[i] = Character.toUpperCase(c);
                 correct = true;
-            }
-        }
-        if (!correct) guessed++;
-        return correct;
+      }
     }
+    if (!correct) guessed++;
+    return correct;
+  }
 
     public boolean hasWon() {
         return Arrays.equals(word, guesses);
@@ -46,23 +54,36 @@ public class Hangman {
         System.out.println();
       }
     }
+  // public void hangman(){
+  //   switch (guessed){
+      
+  //   }
+  // }
   public void game(){
     Scanner scanner = new Scanner(System.in);
     display();
     while (!hasWon() && !hasLost()) {
       System.out.print("Guess a letter: ");
       char c = scanner.nextLine().charAt(0);
-      if (!guess(c)) {
-        System.out.println("Incorrect!");
+      if (letters.indexOf(Character.toUpperCase(c))!=-1){
+        System.out.println("This has been guessed before");
       }
-      display();
+      else{
+        if (!guess(c)) {
+          System.out.println("Incorrect!");
+        }
+        display();
+      }
+      System.out.println();
     }
     if (hasWon()) {
       System.out.println("You win!");
     } else {
       System.out.println("You lose!");
     }
+    
   }
 
     
 }
+
--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import com.theokanning.openai.*;
+import com.theokanning.openai.completion.*;
+
 
 public class Main{
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter your word: ");
+    System.out.print("Enter your theme: ");
+    
+    String theme = scanner.nextLine();
+    
+
     String word = scanner.nextLine();
     Hangman hangman = new Hangman(word,5);
     hangman.game();
